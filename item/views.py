2e3883d9535c1e65e1c3298911cc0386f76d5ed7
Kripesh Poudel <pkripesh345@gmail.com>
2023-12-14 18:04:20 +0545

    minor changes in search and filter

--- item/views.py
+++ item/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Item
+from django.db.models import Q
+from .models import Item, category
 from django.contrib.auth.decorators import login_required
 from .forms import NewItemForm, EditItemForm
 
+
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('Category' , 0)
+    categories = category.objects.all()
+    items = Item.objects.filter(is_available=True)
+
+    if category_id:
+        items = items.filter(Category__id=category_id)
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query) )
+
+    return render(request, 'item/items.html', {
+        'items':items,
+        'query': query,
+        'categories':categories,
+        'category_id': int(category_id),
+    })
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
