3c97dc0f56724dfb9be244ac73ec908b37c82b16
Kripesh Poudel <pkripesh345@gmail.com>
2023-12-18 17:22:18 +0545

    footer route added

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,9 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
+    path('faq/', views.faq, name='faq'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('terms/', views.terms, name='terms'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name="core/login.html", authentication_form=LoginForm), name='login'), 
 ]
--- core/views.py
+++ core/views.py
@@ -16,6 +16,15 @@ def index(request):
 def contact(request):
     return render(request, 'core/contact.html')
 
+def faq(request):
+    return render(request ,'core/faq.html')
+
+def privacy(request):
+    return render(request ,'core/privacy.html')
+
+def terms(request):
+    return render(request ,'core/terms.html')
+
 def signup(request):
     if request.method=='POST':
         form = SignupForm (request.POST)
