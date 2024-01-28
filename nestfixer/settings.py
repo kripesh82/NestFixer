f767369401363a6094df0525e92a28fc5a656a1e
Ukran004 <110590995+Ukran004@users.noreply.github.com>
2024-01-28 21:26:43 +0545

    UI color and font changed

--- nestfixer/settings.py
+++ nestfixer/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'nestfixer',
         'USER':'postgres',
-        'PASSWORD':'iphone53g@',
+        'PASSWORD':'Achinty@1',
         'HOST':'localhost',
     }
 }
