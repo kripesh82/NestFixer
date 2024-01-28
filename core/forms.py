9bdab10dbcc948ccca2b679c492d3b217a5ab1b5
kripesh82 <pkripesh345@gmail.com>
2024-01-28 18:29:41 +0545

    UI changes in input fields

--- core/forms.py
+++ core/forms.py
@@ -8,9 +8,9 @@ class CustomPasswordChangeForm(PasswordChangeForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Customize the widget attributes for password fields
-        self.fields['old_password'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl'
-        self.fields['new_password1'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl'
-        self.fields['new_password2'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl'
+        self.fields['old_password'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl bg-white border border-gray-400 text-gray-900 text-sm rounded-lg  focus:border-blue-500'
+        self.fields['new_password1'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl bg-white border border-gray-400 text-gray-900 text-sm rounded-lg  focus:border-blue-500'
+        self.fields['new_password2'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl bg-white border border-gray-400 text-gray-900 text-sm rounded-lg  focus:border-blue-500'
 
 
 class EditProfileForm(UserChangeForm):
@@ -21,23 +21,23 @@ class EditProfileForm(UserChangeForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Customize form fields if needed
-        self.fields['username'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl'
+        self.fields['username'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl bg-white border border-gray-400 text-gray-900 text-sm rounded-lg  focus:border-blue-500'
         self.fields['username'].widget.attrs['placeholder'] = 'Enter your username'
 
-        self.fields['email'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl'
+        self.fields['email'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl bg-white border border-gray-400 text-gray-900 text-sm rounded-lg  focus:border-blue-500'
         self.fields['email'].widget.attrs['placeholder'] = 'Enter your email'
 
         
         # Check for the correct field names for password change
         if 'password' in self.fields:
-            self.fields['password'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl'
+            self.fields['password'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl bg-white border border-gray-400 text-gray-900 text-sm rounded-lg  focus:border-blue-500'
             self.fields['password'].widget.attrs['placeholder'] = 'Enter Old Password'
 
         if 'new_password1' in self.fields:
-            self.fields['new_password1'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl'
+            self.fields['new_password1'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl bg-white border border-gray-400 text-gray-900 text-sm rounded-lg  focus:border-blue-500'
             self.fields['new_password1'].widget.attrs['placeholder'] = 'Enter New Password'
         if 'new_password2' in self.fields:
-            self.fields['new_password2'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl'
+            self.fields['new_password2'].widget.attrs['class'] = 'w-full py-4 px-6 rounded-xl bg-white border border-gray-400 text-gray-900 text-sm rounded-lg  focus:border-blue-500'
             self.fields['new_password1'].widget.attrs['placeholder'] = 'Confirm New Password'
 
 
@@ -48,12 +48,12 @@ class LoginForm(AuthenticationForm):
     
     username=forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Enter Username',
-        'class': 'w-full py-4 px-6 rounded-xl'
+        'class': 'w-full py-4 px-6 rounded-xl bg-white border border-gray-400 text-gray-900 text-sm rounded-lg  focus:border-blue-500'
     }))
     
     password=forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Enter Password',
-        'class': 'w-full py-4 px-6 rounded-xl'
+        'class': 'w-full py-4 px-6 rounded-xl bg-white border border-gray-400 text-gray-900 text-sm rounded-lg  focus:border-blue-500'
     }))
      
 
@@ -65,22 +65,22 @@ class SignupForm(UserCreationForm):
 
     username=forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Enter Username',
-        'class': 'w-full py-4 px-6 rounded-xl'
+        'class': 'w-full py-4 px-6 rounded-xl bg-white border border-gray-400 text-gray-900 text-sm rounded-lg  focus:border-blue-500'
     }))
 
     email=forms.CharField(widget=forms.EmailInput(attrs={
         'placeholder': 'Enter Email',
-        'class': 'w-full py-4 px-6 rounded-xl'
+        'class': 'w-full py-4 px-6 rounded-xl bg-white border border-gray-400 text-gray-900 text-sm rounded-lg  focus:border-blue-500'
     }))
 
     password1=forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Enter Password',
-        'class': 'w-full py-4 px-6 rounded-xl'
+        'class': 'w-full py-4 px-6 rounded-xl bg-white border border-gray-400 text-gray-900 text-sm rounded-lg  focus:border-blue-500'
     }))
 
     password2=forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Confirm Password',
-        'class': 'w-full py-4 px-6 rounded-xl'
+        'class': 'w-full py-4 px-6 rounded-xl bg-white border border-gray-400 text-gray-900 text-sm rounded-lg  focus:border-blue-500'
     }))
 
     def clean(self):
